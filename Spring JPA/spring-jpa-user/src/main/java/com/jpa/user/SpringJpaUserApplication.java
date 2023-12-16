9eff2525bdc1ed01d4fc1acc662ebdf29f57e2a9
ssafy10-dykim <dy.jo.kim@gmail.com>
2023-12-16 15:48:31 +0900

    create rest jpa user

---
+++ Spring JPA/spring-jpa-user/src/main/java/com/jpa/user/SpringJpaUserApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaUserApplication.class, args);
+	}
+
+}
---
+++ Spring JPA/spring-jpa-user/src/test/java/com/jpa/user/repository/UserTest.java
@@ -0,0 +1,8 @@
+package com.jpa.user.repository;
+
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test") // application-test.properties 사용
+public class UserTest {
+
+}
